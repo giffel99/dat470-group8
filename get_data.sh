004d1d52d551c400322f144a4b3f2b80e9ccead1
Felicia Hjalmarsson <felicia.hjalmarsson@gmail.com>
2023-03-24 16:40:57 +0100

    Clearer output and modified clock frequency

--- get_data.sh
+++ get_data.sh
@@ -8,27 +8,26 @@ num_cores=$((cores_per_socket*sockets))
 
 
 # Get the type of CPUs and clock frequency
-cpu_info=$(lscpu | grep -E "(Model name)")
+cpu_info=$(lscpu | grep 'Model name' | awk -F: '{$1="";print}' | awk '{$1=$1;print}')
+cpu_clock_low=$( cat /proc/cpuinfo | grep 'cpu MHz' | sort | head -n 1 | awk -F: '{$1="";print}')
+cpu_clock_high=$( cat /proc/cpuinfo | grep 'cpu MHz' | sort | tail -n 1 | awk -F: '{$1="";print}')
 
 # Get disk usage and total available for the filesystems /data and /datainbackup
 disk_usage_data=$(df -h --output=used,size /data | tail -n 1)
 disk_usage_datainbackup=$(df -h --output=used,size /datainbackup | tail -n 1)
 
-# To access on 'markov' and 'shannon' -> /bayes_datainbackup/
 
 # Get memory usage of current login shell
 shell_pid=$$
-shell_mem_usage=$(ps -o rss= -p $shell_pid)
-
-# Or as
-shell_mem_usage=$(pmap $shell_pid | tail -n 1)
+shell_mem_usage=$(pmap $shell_pid | tail -n 1 | awk '{print $2}')
 
 
 # Print all the information gathered
 echo "1. Number of cores: $num_cores (with HyperThreading)"
-echo "2. CPU information: $cpu_info"
-echo "3. Disk usage (used, total):"
+echo "2. Type of CPU: $cpu_info"
+echo "3. Clock frequencies (cpu MHz) between: $cpu_clock_low   - $cpu_clock_high"
+echo "4. Disk usage (used, total):"
 echo "   /data: $disk_usage_data"
 echo "   /datainbackup: $disk_usage_datainbackup"
-echo "4. Memory usage of login shell: ${shell_mem_usage}KB"
+echo "5. Memory usage of login shell: ${shell_mem_usage}KB"
 
