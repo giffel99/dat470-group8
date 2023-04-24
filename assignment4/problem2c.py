f3cd711576046feac860e636946d2c091aecacb0
marcuscarlssoon <marcuscarlsson00@gmail.com>
2023-04-24 15:55:49 +0200

    Implementation of assignment 4 task 2d + small refactor of 2c

--- assignment4/problem2c.py
+++ assignment4/problem2c.py
@@ -1,6 +1,6 @@
 import findspark
+
 findspark.init()
-import random
 from pyspark import SparkContext
 import argparse
 
@@ -10,15 +10,16 @@ def get_bin_index(value, min_value, max_value, num_bins):
     bin_index = int((value - min_value) // bin_size)
     return min(bin_index, num_bins - 1)
 
+
 def get_bin_range(bin_index, min_value, max_value, num_bins):
     bin_size = (max_value - min_value) / num_bins
     lower_bound = min_value + bin_index * bin_size
     upper_bound = lower_bound + bin_size
     return f"{lower_bound:.2f} - {upper_bound:.2f}"
 
-def main(data_path, min_value, max_value, num_bins):
 
-    sc = SparkContext(master = 'local[2]')
+def main(data_path, min_value, max_value, num_bins):
+    sc = SparkContext(master='local[2]')
 
     # Constructing an RDD from the data file 
     data = sc.textFile(data_path)
@@ -30,8 +31,8 @@ def main(data_path, min_value, max_value, num_bins):
     # ReduceByKey will add the counts together and sortByKey will ensure the output is in the right order. 
     histogram = (
         values.map(lambda value: (get_bin_index(value, min_value, max_value, num_bins), 1))
-              .reduceByKey(lambda a, b: a + b)
-              .sortByKey()
+        .reduceByKey(lambda a, b: a + b)
+        .sortByKey()
     )
 
     # Collect the bin counts for the histogram and printing it with value ranges for each bin
@@ -50,4 +51,3 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     main(args.data_path, args.min_value, args.max_value, args.num_bins)
-
---
+++ assignment4/problem2d.py
@@ -0,0 +1,65 @@
+from pyspark import SparkContext
+import argparse
+
+
+def main(data_path, min_value, max_value, num_bins):
+    sc = SparkContext(master='local[2]')
+
+    # Constructing an RDD from the data file
+    data = sc.textFile(data_path)
+
+    # Extracting the value field in the data
+    values = data.map(lambda line: float(line.split('\t')[2]))
+
+    # Calculate the bin counts for the histogram
+    histogram = (
+        values.map(lambda value: (get_bin_index(value, min_value, max_value, num_bins), 1))
+        .reduceByKey(lambda a, b: a + b)
+        .sortByKey()
+    )
+
+    # Collect the bin counts for the histogram
+    histogram_counts = histogram.collect()
+
+    # Calculate the total count
+    total_count = sum(count for _, count in histogram_counts)
+
+    # Calculate the cumulative sum for each histogram bin
+    bins_cumulative_sums = []
+    cumulative_sum = 0
+    for bin_range, count in histogram_counts:
+        cumulative_sum += count
+        bins_cumulative_sums.append((bin_range, cumulative_sum))
+
+    # Find the median bin
+    median_count = total_count / 2
+    median_bin_range = next(
+        (bin_range for bin_range, cumulative_count in bins_cumulative_sums if cumulative_count >= median_count),
+        None)
+
+    # Approximate the median by taking the mean of the median bin range
+    lower, upper = [float(x) for x in median_bin_range.split(" - ")]
+    approx_median = (lower + upper) / 2
+
+    print(f"Approximated Median: {approx_median}")
+
+
+def get_bin_index(value, min_value, max_value, num_bins):
+    bin_size = (max_value - min_value) / num_bins
+    bin_index = int((value - min_value) // bin_size)
+    bin_index = min(bin_index, num_bins - 1)
+
+    lower = min_value + bin_index * bin_size
+    upper = min_value + (bin_index + 1) * bin_size
+    return f"{lower} - {upper}"
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="PySpark Approximated Median")
+    parser.add_argument('--data_path', type=str, required=True, help="Path to the input data file")
+    parser.add_argument('--num_bins', type=int, default=10, help='Number of bins for histogram')
+    parser.add_argument('--min_value', type=float, required=True, help='Minimum value for data')
+    parser.add_argument('--max_value', type=float, required=True, help='Maximum value for data')
+    args = parser.parse_args()
+
+    main(args.data_path, args.min_value, args.max_value, args.num_bins)
