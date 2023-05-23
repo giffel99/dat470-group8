47baee92fc091f5f34bbeee848419ee1ca9d9e1c
Felicia Hjalmarsson <felicia.hjalmarsson@gmail.com>
2023-05-23 12:56:47 +0200

    added executions times for different number of cores as comments to use for speedup plot

--- assignment7/problem1c.py
+++ assignment7/problem1c.py
@@ -4,6 +4,7 @@ from pyspark import SparkContext
 import numpy as np
 import math
 from random import randrange
+import time
 
 def random_32_bits(input_size):
     np.random.seed(1)
@@ -45,7 +46,7 @@ def set_alpha_m(m):
     else:
         return 0.7213 / ( 1 + ( 1.079/m ))
 
-def hyperloglog_partition(partition, broadcast_A):
+def hyperloglog_on_partition(partition, broadcast_A):
     n_approx = 0
     l = 8
     m = 2**l
@@ -65,14 +66,14 @@ def hyperloglog_partition(partition, broadcast_A):
     yield n_approx
 
 def main():
-    sc = SparkContext(master = 'local[4]')
+    sc = SparkContext(master = 'local[32]')
 
     A = random_32_bits(8)
 
     # Creating a broadcast variable to distribute 'A' across the Spark cluster
     A_broadcasted = sc.broadcast(A)
 
-    random_numbers = [randrange(2**64) for i in range(10000)]
+    random_numbers = [randrange(2**64) for i in range(1000000)]
 
     # Constructing an RDD of the random numbers
     rdd = sc.parallelize(random_numbers)
@@ -81,9 +82,30 @@ def main():
     total_distinct = rdd.distinct().count()
 
     # Performing hyperloglog algorithm to every partition of the RDD and then combining the results 
-    n_hat = rdd.mapPartitions(lambda partition: hyperloglog_partition(partition, A_broadcasted)).reduce(lambda a,b: a+b)
+    n_hat = rdd.mapPartitions(lambda partition: hyperloglog_on_partition(partition, A_broadcasted)).reduce(lambda a,b: a+b)
     print(total_distinct)
     print(n_hat)
 
 if __name__ == '__main__':
+    start = time.time()
     main()
+    end = time.time()
+    total_time = end - start
+
+    print(f'Total execution time is: {total_time}')
+
+# Execution times for 100000 random numbers
+# Total execution time is: 13.93044662475586 (1 core)
+# Total execution time is: 10.718540668487549 (2 cpres)
+# Total execution time is: 8.37300419807434 (4 cores)
+# Total execution time is: 7.9815497398376465 (8 cores)
+# Total execution time is: 7.747605323791504 (16 cores)
+# Total execution time is: 8.63371753692627 (32 cores)
+
+# Execution times for 1000000 random numbers
+# Total execution time is: 82.16899871826172 (1 core)
+# Total execution time is: 38.74206256866455 (2 cpres)
+# Total execution time is: 23.30802869796753 (4 cores)
+# Total execution time is: 16.739031314849854 (8 cores)
+# Total execution time is: 13.05227255821228 (16 cores)
+# Total execution time is: 12.738852977752686 (32 cores)
